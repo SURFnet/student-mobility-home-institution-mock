87bf69cdfdf2f34ec144375c14f54d6815d793df
Okke Harsta <oharsta@zilverline.com>
2020-12-04 15:34:02 +0100

    Added logging

--- src/main/java/home/api/OfferingEndpoint.java
+++ src/main/java/home/api/OfferingEndpoint.java
@@ -1,10 +1,10 @@
 package home.api;
 
 import io.micrometer.core.instrument.util.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +14,12 @@ import java.io.IOException;
 @RestController
 public class OfferingEndpoint {
 
+    private static final Log LOG = LogFactory.getLog(OfferingEndpoint.class);
+
     //https://open-education-api.github.io/specification/v4/docs.html#tag/offerings
     @GetMapping(value = "/offerings/{offeringId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String offerings(@PathVariable("offeringId") String offeringId) throws IOException {
+        LOG.debug("Returning offering with ID: " + offeringId);
         return IOUtils.toString(new ClassPathResource("/data/offering.json").getInputStream());
     }
 }
