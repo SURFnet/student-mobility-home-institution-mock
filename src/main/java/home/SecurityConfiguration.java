9cbd37d67d763889926e6333e8c7a558dd8c132d
Okke Harsta <oharsta@zilverline.com>
2020-12-18 10:50:30 +0100

    Added oauth2 and basic security for offering endpoint

--- src/main/java/home/SecurityConfiguration.java
+++ src/main/java/home/SecurityConfiguration.java
@@ -1,26 +1,73 @@
 package home;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authz -> authz
-                        .antMatchers(HttpMethod.GET, "/offerings/**", "/actuator/health", "/actuator/info")
-                        .permitAll()
-                        .antMatchers(HttpMethod.GET, "/persons/**")
-                        .hasAuthority("SCOPE_openid")
-                        .anyRequest().authenticated())
 
-                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    @Order(1)
+    @Configuration
+    public static class OAuth2SecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers(HttpMethod.GET, "/actuator/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.requestMatchers().antMatchers("/actuator/**", "/persons/**", "/oauth2/offerings/**")
+                    .and()
+                    .authorizeRequests(authz -> authz
+                            .antMatchers(HttpMethod.GET)
+                            .hasAuthority("SCOPE_openid")
+                            .anyRequest().authenticated())
+
+                    .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken())
+                    .sessionManagement(sessionManagement ->
+                            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        }
+
+    }
+
+    @Order
+    @Configuration
+    public static class BasicSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Value("${security.user.name}")
+        private String user;
+
+        @Value("${security.user.password}")
+        private String password;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser(user).password(String.format("{noop}%s",password)).authorities("ROLE_USER");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/basic/offerings/**")
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .csrf().disable()
+                    .addFilterBefore(new BasicAuthenticationFilter(authenticationManager()), BasicAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers("/**").hasRole("USER");
+        }
     }
 }
--- src/test/java/home/api/OfferingEndpointTest.java
+++ src/test/java/home/api/OfferingEndpointTest.java
@@ -1,32 +1,84 @@
 package home.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.nimbusds.jose.JOSEException;
 import home.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import org.springframework.beans.factory.annotation.Value;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_FORBIDDEN;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 import static org.hamcrest.Matchers.equalTo;
 
 public class OfferingEndpointTest extends AbstractIntegrationTest {
 
+    @Value("${security.user.name}")
+    private String user;
+
+    @Value("${security.user.password}")
+    private String password;
+
     @Test
-    void offerings() throws NoSuchProviderException, NoSuchAlgorithmException, JOSEException, JsonProcessingException {
+    void offeringsNoSecurity() {
         given()
                 .when()
-                .get("/offerings/1")
+                .get("/nope/offerings/1")
                 .then()
                 .statusCode(SC_OK)
                 .body("abbreviation", equalTo("Test-INFOMQNM-20FS"));
+    }
+
+    @Test
+    void offeringsBasicAuth() {
+        given()
+                .when()
+                .auth().preemptive().basic(user, password)
+                .get("/basic/offerings/1")
+                .then()
+                .statusCode(SC_OK)
+                .body("abbreviation", equalTo("Test-INFOMQNM-20FS"));
+    }
+
+    @Test
+    void offeringsBasicAuthForbidden() {
+        given()
+                .when()
+                .get("/basic/offerings/1")
+                .then()
+                .statusCode(SC_FORBIDDEN);
+
+    }
+
+    @Test
+    void offeringsOauth2() throws Exception {
+        given()
+                .when()
+                .auth().oauth2(opaqueAccessToken(true))
+                .get("/oauth2/offerings/1")
+                .then()
+                .statusCode(SC_OK)
+                .body("abbreviation", equalTo("Test-INFOMQNM-20FS"));
+    }
+
+    @Test
+    void offeringInvalidToken() throws Exception {
+        given()
+                .when()
+                .auth().oauth2(opaqueAccessToken(false))
+                .get("/oauth2/offerings/1")
+                .then()
+                .statusCode(SC_UNAUTHORIZED);
+    }
 
 
+    @Test
+    void offeringsNoAuth() {
+        given()
+                .when()
+                .get("/nope/offerings/1")
+                .then()
+                .statusCode(SC_OK)
+                .body("abbreviation", equalTo("Test-INFOMQNM-20FS"));
     }
 
 }
